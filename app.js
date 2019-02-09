88cd3b2eafbd6dd8e65096cc68138c7374489f7d
Shailendra <shailendrashuklapro@gmail.com>
2019-02-09 10:12:23 -0500

    Code cleaned and indented

--- app.js
+++ app.js
@@ -4,30 +4,41 @@ let request = new Request(`https://api.etherscan.io/api?module=stats&action=ethp
 
 fetch(request).then(response => response.json())
 .then(data => {
+
     let res = data.result;
     document.getElementById('priceusd').textContent = res.ethusd;
     document.getElementById('pricebtc').textContent = res.ethbtc;
-})
+
+});
 
 fetch(`https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=${YourApiKeyToken}`)
     .then(response => response.json())
     .then(response => {
+
         console.log(response);
         document.getElementById('lastBlock').textContent = parseInt(response.result);
         return response.result;
+
     })
     .then(data => {
+
         return fetch(`https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=${data}&boolean=true&apikey=${YourApiKeyToken}`)
+
     })
     .then(response => response.json())
     .then(data => {
+
         var result = data.result;
+
         document.getElementById('blockSize').textContent = `${parseInt(result.size)} Bytes`;
         document.getElementById('blockDifficulty').textContent = parseInt(result.difficulty);
         document.getElementById('transactions').textContent = result.transactions.length;
+
         var str = `${parseInt(result.gasUsed)} (${(parseInt(result.gasUsed)*100)/parseInt(result.gasLimit)}%)`;
+
         document.getElementById('gasUsed').textContent = str;
         document.getElementById('gasLimit').textContent = parseInt(result.gasLimit);
+
         console.log(result);
     });
 
